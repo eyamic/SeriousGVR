bf246683ebe48c9665cc9550e86efa264713450b
ZZZR0925 <ruizhang925@163.com>
2024-02-20 14:56:08 +0000

    开关门动画和用E开关门

---
+++ SeriousGame2/Assets/Rui Zhang/Assets/Scripts/DoorOpenClose.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenClose : MonoBehaviour
+{
+    private Animator anim;
+
+    private bool isOpen = false;//门的状态
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!isOpen)//门是关的
+            {
+                anim.SetTrigger("Open");
+                isOpen = true;//设为“打开的门”
+            }
+            else//门是开的
+            {
+                anim.SetTrigger("Close");
+                isOpen = false;//设为“关上的门”
+            }
+        }
+    }
+
+    //private void OnTriggerStay
+    //{
+        //if(other.CompareTag("Player"))
+       // {
+          //isOpen = true;
+       // }
+        
+    //private void OnTriggerExit
+    //{
+        //if(other.CompareTag("Player"))
+        //{
+          //isOpen = false;
+        //}    
+        
+    //}
+}
