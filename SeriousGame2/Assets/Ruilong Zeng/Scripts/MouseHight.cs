b4ec56a0302618cbbe5755c4700f14486d2a78e7
Eyamic1 <1374502833@qq.com>
2024-02-26 13:53:18 +0000

    add some

--- SeriousGame2/Assets/Ruilong Zeng/Scripts/MouseHight.cs
+++ SeriousGame2/Assets/Ruilong Zeng/Scripts/MouseHight.cs
@@ -3,16 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class MouseHight : MonoBehaviour
 {
-    public RectTransform uiTansform;
-    public RectTransform GuaizhangTansform;
+    [Header("UI Part")] public RectTransform uiTansform;
+    public GameObject talkcanvs;
+    public Button closeButton;
+
+    [Header("Interaction Part")] public Transform handTransform;
+    public PlayerCamera playerCamera;
+    
+
     private GameObject highlightedObject;
+    private bool canRotateVertically = true;
+
+   
+
     private void Update()
     {
+        closeButton.onClick.AddListener(Close);
         RaycastHit hit;
-        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
-        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
         if (Physics.Raycast(ray, out hit))
         {
             GameObject hitObject = hit.collider.gameObject;
@@ -27,15 +39,31 @@ public class MouseHight : MonoBehaviour
                     ClearHighlight();
                     highlightedObject = hitObject;
                     outlineComponent.enabled = true;
-                    
+
                     uiTansform.gameObject.SetActive(true);
+                }
+
+                if (hitObject.CompareTag("CanPickup"))
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        PickUpItem(hitObject);
+                    }
+                }
+
+                if (hitObject.CompareTag("CanTalk"))
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        talkcanvs.SetActive(true);
+                    }
                 }
             }
             else
             {
                 // 如果物体上没有 Outline 组件，则清除高亮
                 ClearHighlight();
-                
+
                 uiTansform.gameObject.SetActive(false);
             }
         }
@@ -45,6 +73,26 @@ public class MouseHight : MonoBehaviour
             ClearHighlight();
             uiTansform.gameObject.SetActive(false);
         }
+        
+    }
+
+    void Close()
+    {
+        talkcanvs.SetActive(false);
+    }
+
+    private void PickUpItem(GameObject gamobject)
+    {
+        Collider itemCollider = gamobject.GetComponent<Collider>();
+        if (itemCollider != null)
+        {
+            itemCollider.enabled = false;
+        }
+        playerCamera.SetMoveUpDownEnabled(false);
+
+        gamobject.transform.position = handTransform.position;
+        gamobject.transform.rotation = handTransform.rotation;
+        gamobject.gameObject.transform.SetParent(handTransform);
     }
 
     private void ClearHighlight()
